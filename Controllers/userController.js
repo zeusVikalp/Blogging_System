5ed9c0899082d5d7d4ee53a04ea66498de8b9cab
zeusVikalp <100758326+zeusVikalp@users.noreply.github.com>
2022-05-28 23:03:41 +0530

    2 done

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -28,13 +28,18 @@ async function getBlog(request,response){
     try {
         let search = request.query.title;
         let authSearch = request.query.author
-        let res = await userModel.find( { $and: [ { title: title}, { author : author } ] } )
+        let res = await userModel.find( { $and: [ { title:{$regex : search, $options : '$i'} }, { author :{$regex:authSearch, $options : '$i'} } ] } )
         // let res1 = await userModel.find
+        if(res.length){
         response.status(200).json({
             status:"Success",
             message: "Search Successfully",
-            user : res || res2
+            user : res
         })  
+    }
+    else{
+        res.send("Data not found")
+    }
     } catch (error) {
         console.log(error)
         response.status(400).json({
@@ -47,27 +52,26 @@ async function getBlog(request,response){
     
 }
 
-async function authPost(request,response){
-    try {
-        let body = request.body
-        let res = await authorModel.insertMany([body]);
-        response.status(200).json({
-            status: "author Successfull",
-            message: "author post successfully",
-            authpost: res
-        })
-    } catch (error) {
-        console.log(error)
-        response.status(400).json({
-            status: "Error"
-        })
+// async function authPost(request,response){
+//     try {
+//         let body = request.body
+//         let res = await authorModel.insertMany([body]);
+//         response.status(200).json({
+//             status: "author Successfull",
+//             message: "author post successfully",
+//             authpost: res
+//         })
+//     } catch (error) {
+//         console.log(error)
+//         response.status(400).json({
+//             status: "Error"
+//         })
         
-    }
-}
+//     }
+// }
 
 module.exports = {
     createPost,
-    getBlog,
-    authPost
+    getBlog
 
 }
--- Routes/app.js
+++ Routes/app.js
@@ -4,7 +4,7 @@ const app = express();
 app.use(express.json())
 
 app.post("/post",userController.createPost);
-app.post("/authpost", userController.authPost )
+// app.get("/authpost", userController.authPost )
 app.get("/user",userController.getBlog);
 
 module.exports = app
